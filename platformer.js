6e4b71c15ed321069ad972c2b050a2ee582b145c
sdcosta <sdcosta@gmail.com>
2014-03-12 15:53:25 -0400

    Implemented REST Server api
    navigate to ~/Mindwave and run (python EEGData.py)
    navigate to ~/FightAndFlight/ run(python -m SimpleHTTPServer 8000)

--- platformer.js
+++ platformer.js
@@ -6,7 +6,8 @@
 //
 // This is the example from the website homepage, it consists
 // a simple, non-animated platformer with some enemies and a 
-// target for the player.
+// target for the player
+var oneInQueue = false;
 window.addEventListener("load",function() {
 
 // Set up an instance of the Quintus engine  and include
@@ -173,24 +174,45 @@ Q.Sprite.extend("Player",{
       this.p.left = false;
     }
 
-    if(Q.inputs['S']) {
-      this.p.focus = (this.p.focus + 1) < 100 ? this.p.focus + 1 : 100;
-    }else{
-      this.p.focus = (this.p.focus - 1) > 0 ? this.p.focus - 1 : 0;
+    if (!oneInQueue)
+    {
+      oneInQueue = true;
+      var user = this;
+      $.ajax({
+        type:  'GET',
+        dataType: "jsonp",
+        url:  'http://localhost:8080/ESenseData.json',
+        success: function(response) {
+            user.p.focus = parseInt(response.attention);
+            user.p.calm = parseInt(response.meditation);            
+          },
+        error: function(xhr, textStatus, errorThrown)
+        {
+          console.log(xhr.toString());
+          console.log(textStatus);
+          console.log(errorThrown);
+          if(Q.inputs['S']) {
+            user.p.focus = (user.p.focus + 1) < 100 ? user.p.focus + 1 : 100;
+          }else{
+            user.p.focus = (user.p.focus - 1) > 0 ? user.p.focus - 1 : 0;
+          }
+          if(Q.inputs['P']) {
+            user.p.calm = (user.p.calm + 1) < 100 ? user.p.calm + 1 : 100;
+          }else{
+            user.p.calm = (user.p.calm - 1) > 0 ? user.p.calm - 1 : 0;
+          }
+        },
+        complete: function(jqXHR, textStatus){
+          oneInQueue = false;
+        }
+        });
+      if(this.p.calm >= 50){
+          this.p.flying = true;
+        }else{
+          this.p.flying = false;
+        }
     }
-    if(Q.inputs['P']) {
-      this.p.calm = (this.p.calm + 1) < 100 ? this.p.calm + 1 : 100;
-    }else{
-      this.p.calm = (this.p.calm - 1) > 0 ? this.p.calm - 1 : 0;
-    }
-    if(this.p.calm >= 90){
-      this.p.flying = true;
-    }else{
-      this.p.flying = false;
-    }
-
-
-
+    
     Q.stageScene('hud', 3, this.p);
 
     if(this.p.onLadder) {
